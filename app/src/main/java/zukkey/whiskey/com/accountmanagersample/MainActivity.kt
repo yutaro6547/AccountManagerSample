1c408c6393c9967874830cb0790f0724a39e047c
zukkey <y_vkwi_27_hunhun@yahoo.co.jp>
2018-10-25 20:45:36 +0900

    emailパターンで取得できるように

--- app/src/main/java/zukkey/whiskey/com/accountmanagersample/MainActivity.kt
+++ app/src/main/java/zukkey/whiskey/com/accountmanagersample/MainActivity.kt
@@ -1,12 +1,30 @@
 package zukkey.whiskey.com.accountmanagersample
 
-import android.support.v7.app.AppCompatActivity
+import android.accounts.AccountManager
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Patterns
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    val emailPattern = Patterns.EMAIL_ADDRESS
+    val accounts = AccountManager.get(this).accounts
+    val emailList = mutableListOf<String>()
+    for (account in accounts) {
+      if (emailPattern.matcher(account.name).matches()) {
+        emailList.add(account.name)
+      }
+    }
+    val emails = ArrayAdapter<String>(this, R.layout.item_email_list, emailList)
+    val editText = findViewById<AutoCompleteTextView>(R.id.edit_text)
+    editText.setAdapter(emails)
+    editText.threshold = 1
   }
 }
